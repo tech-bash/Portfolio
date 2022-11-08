4e9ec0dcccba9d47a7d6753f2ed1f1f9e4415c27
Bhavy airi <airiraghav@gmail.com>
2022-11-08 18:49:59 +0530

    Add files via upload

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Main, Head, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html className='dark' >
+      <Head>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+
+}
+
